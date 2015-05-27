137bda9d307e6abc1439a6fa04a6b4f14a96e528
Ka-Hing Cheung <khc@bouncestorage.com>
2015-05-26 18:25:40 -0700

    run a subset of swift tests in travis
    this uses the filesystem blobstore in jclouds which suffers from
    a number of issues, some of which have pending pr in jclouds

--- src/test/resources/run-swift-tests.sh
+++ src/test/resources/run-swift-tests.sh
@@ -22,10 +22,103 @@ function cleanup {
 
 trap cleanup EXIT
 
-SWIFT=$(sudo docker run -d pbinkley/docker-swift)
-SWIFT_IP=$(sudo docker inspect  -f '{{ .NetworkSettings.IPAddress }}' $SWIFT)
+SWIFT_DOCKER_TESTS=$(echo \
+        test.functional.tests:TestContainerPaths \
+        test.functional.tests:TestFile.testCopy \
+        test.functional.tests:TestFile.testCopyAccount \
+        test.functional.tests:TestFileComparison \
+        test.functional.tests:TestFile.testMetadataNumberLimit \
+        test.functional.tests:TestFile.testMetadataLengthLimits \
+        test.functional.tests:TestSlo.test_slo_copy_account \
+        test.functional.tests:TestSlo.test_slo_copy_the_manifest_account \
+        test.functional.tests:TestFile.testRangedGets \
+        test.functional.tests:TestFile.testRangedGetsWithLWSinHeader \
+)
 
-cat > target/swiftproxy-saio.conf <<EOF
+
+SWIFT_TESTS=$(echo \
+        test.functional.tests:TestAccountEnv \
+        test.functional.tests:TestAccountDev \
+        test.functional.tests:TestAccountDevUTF8 \
+        test.functional.tests:TestAccountNoContainersEnv \
+        test.functional.tests:TestAccountNoContainers \
+        test.functional.tests:TestAccountNoContainersUTF8 \
+        test.functional.tests:TestContainerEnv \
+        test.functional.tests:TestContainerDev \
+        test.functional.tests:TestContainerDevUTF8 \
+        test.functional.tests:TestContainer.testContainerNameLimit \
+        test.functional.tests:TestContainer.testFileThenContainerDelete \
+        test.functional.tests:TestContainer.testPrefixAndLimit \
+        test.functional.tests:TestContainer.testCreate \
+        test.functional.tests:TestContainer.testContainerFileListOnContainerThatDoesNotExist \
+        test.functional.tests:TestContainer.testCreateOnExisting \
+        test.functional.tests:TestContainer.testSlashInName \
+        test.functional.tests:TestContainer.testDelete \
+        test.functional.tests:TestContainer.testDeleteOnContainerThatDoesNotExist \
+        test.functional.tests:TestContainer.testDeleteOnContainerWithFiles \
+        test.functional.tests:TestContainer.testFileCreateInContainerThatDoesNotExist \
+        test.functional.tests:TestContainer.testLastFileMarker \
+        test.functional.tests:TestContainer.testContainerFileList \
+        test.functional.tests:TestContainer.testMarkerLimitFileList \
+        test.functional.tests:TestContainer.testFileOrder \
+        test.functional.tests:TestContainer.testContainerInfoOnContainerThatDoesNotExist \
+        test.functional.tests:TestContainer.testContainerFileListWithLimit \
+        test.functional.tests:TestContainer.testTooLongName \
+        test.functional.tests:TestContainer.testContainerExistenceCachingProblem \
+        test.functional.tests:TestFile.testCopy404s \
+        test.functional.tests:TestFile.testCopyNoDestinationHeader \
+        test.functional.tests:TestFile.testCopyDestinationSlashProblems \
+        test.functional.tests:TestFile.testCopyFromHeader \
+        test.functional.tests:TestFile.testCopyFromHeader404s \
+        test.functional.tests:TestFile.testNameLimit \
+        test.functional.tests:TestFile.testQuestionMarkInName \
+        test.functional.tests:TestFile.testDeleteThen404s \
+        test.functional.tests:TestFile.testBlankMetadataName \
+        test.functional.tests:TestFile.testNoContentLengthForPut \
+        test.functional.tests:TestFile.testDelete \
+        test.functional.tests:TestFile.testBadHeaders \
+        test.functional.tests:TestFile.testEtagWayoff \
+        test.functional.tests:TestFile.testFileCreate \
+        test.functional.tests:TestFile.testDeleteOfFileThatDoesNotExist \
+        test.functional.tests:TestFile.testHeadOnFileThatDoesNotExist \
+        test.functional.tests:TestFile.testMetadataOnPost \
+        test.functional.tests:TestFile.testGetOnFileThatDoesNotExist \
+        test.functional.tests:TestFile.testPostOnFileThatDoesNotExist \
+        test.functional.tests:TestFile.testMetadataOnPut \
+        test.functional.tests:TestFile.testStackedOverwrite \
+        test.functional.tests:TestFile.testTooLongName \
+        test.functional.tests:TestFile.testZeroByteFile \
+        test.functional.tests:TestFile.testEtagResponse \
+        test.functional.tests:TestDlo.test_get_manifest \
+        test.functional.tests:TestDlo.test_get_manifest_document_itself \
+        test.functional.tests:TestDlo.test_get_range \
+        test.functional.tests:TestDlo.test_get_range_out_of_range \
+        test.functional.tests:TestDlo.test_copy \
+        test.functional.tests:TestDlo.test_copy_account \
+        test.functional.tests:TestDlo.test_copy_manifest \
+        test.functional.tests:TestDlo.test_dlo_if_match_get \
+        test.functional.tests:TestDlo.test_dlo_if_none_match_get \
+        test.functional.tests:TestSlo.test_slo_get_simple_manifest \
+        test.functional.tests:TestSlo.test_slo_get_nested_manifest \
+        test.functional.tests:TestSlo.test_slo_ranged_get \
+        test.functional.tests:TestSlo.test_slo_ranged_submanifest \
+        test.functional.tests:TestSlo.test_slo_etag_is_hash_of_etags \
+        test.functional.tests:TestSlo.test_slo_etag_is_hash_of_etags_submanifests \
+        test.functional.tests:TestSlo.test_slo_etag_mismatch \
+        test.functional.tests:TestSlo.test_slo_size_mismatch \
+        test.functional.tests:TestSlo.test_slo_copy \
+        test.functional.tests:TestSlo.test_slo_copy_the_manifest \
+        test.functional.tests:TestSlo.test_slo_get_the_manifest \
+        test.functional.tests:TestSlo.test_slo_head_the_manifest \
+)
+
+: ${TRAVIS:="false"}
+if [ "$TRAVIS" != "true" ]; then
+    SWIFT=$(sudo docker run -d pbinkley/docker-swift)
+    SWIFT_IP=$(sudo docker inspect  -f '{{ .NetworkSettings.IPAddress }}' $SWIFT)
+    SWIFT_TESTS="$SWIFT_TESTS $SWIFT_DOCKER_TESTS"
+
+    cat > target/swiftproxy-saio.conf <<EOF
 swiftproxy.endpoint=http://127.0.0.1:8080
 
 jclouds.provider=openstack-swift
@@ -34,6 +127,15 @@ jclouds.keystone.credential-type=tempAuthCredentials
 jclouds.identity=test:tester
 jclouds.credential=testing
 EOF
+else
+    cat > target/swiftproxy-saio.conf <<EOF
+swiftproxy.endpoint=http://127.0.0.1:8080
+
+jclouds.provider=transient
+jclouds.identity=test:tester
+jclouds.credential=testing
+EOF
+fi
 
 stdbuf -oL -eL $PROXY_BIN --properties $TEST_CONF &
 PROXY_PID=$!
@@ -106,93 +208,11 @@ export PYTHONUNBUFFERED=1
 export NOSE_NOCAPTURE=1
 export NOSE_NOLOGCAPTURE=1
 
+
 if [ $# == 0 ]; then
     SWIFT_TEST_CONFIG_FILE=./virtualenv/etc/swift/test.conf stdbuf -oL -eL ./virtualenv/bin/nosetests -v \
         --logging-level=debug \
-        test.functional.tests:TestAccountEnv \
-        test.functional.tests:TestAccountDev \
-        test.functional.tests:TestAccountDevUTF8 \
-        test.functional.tests:TestAccountNoContainersEnv \
-        test.functional.tests:TestAccountNoContainers \
-        test.functional.tests:TestAccountNoContainersUTF8 \
-        test.functional.tests:TestContainerEnv \
-        test.functional.tests:TestContainerDev \
-        test.functional.tests:TestContainerDevUTF8 \
-        test.functional.tests:TestContainer.testContainerNameLimit \
-        test.functional.tests:TestContainer.testFileThenContainerDelete \
-        test.functional.tests:TestContainer.testPrefixAndLimit \
-        test.functional.tests:TestContainer.testCreate \
-        test.functional.tests:TestContainer.testContainerFileListOnContainerThatDoesNotExist \
-        test.functional.tests:TestContainer.testCreateOnExisting \
-        test.functional.tests:TestContainer.testSlashInName \
-        test.functional.tests:TestContainer.testDelete \
-        test.functional.tests:TestContainer.testDeleteOnContainerThatDoesNotExist \
-        test.functional.tests:TestContainer.testDeleteOnContainerWithFiles \
-        test.functional.tests:TestContainer.testFileCreateInContainerThatDoesNotExist \
-        test.functional.tests:TestContainer.testLastFileMarker \
-        test.functional.tests:TestContainer.testContainerFileList \
-        test.functional.tests:TestContainer.testMarkerLimitFileList \
-        test.functional.tests:TestContainer.testFileOrder \
-        test.functional.tests:TestContainer.testContainerInfoOnContainerThatDoesNotExist \
-        test.functional.tests:TestContainer.testContainerFileListWithLimit \
-        test.functional.tests:TestContainer.testTooLongName \
-        test.functional.tests:TestContainer.testContainerExistenceCachingProblem \
-        test.functional.tests:TestContainerPathsEnv \
-        test.functional.tests:TestContainerPaths \
-        test.functional.tests:TestFile.testCopy \
-        test.functional.tests:TestFile.testCopyAccount \
-        test.functional.tests:TestFile.testCopy404s \
-        test.functional.tests:TestFile.testCopyNoDestinationHeader \
-        test.functional.tests:TestFile.testCopyDestinationSlashProblems \
-        test.functional.tests:TestFile.testCopyFromHeader \
-        test.functional.tests:TestFile.testCopyFromHeader404s \
-        test.functional.tests:TestFile.testNameLimit \
-        test.functional.tests:TestFile.testQuestionMarkInName \
-        test.functional.tests:TestFile.testDeleteThen404s \
-        test.functional.tests:TestFile.testBlankMetadataName \
-        test.functional.tests:TestFile.testMetadataNumberLimit \
-        test.functional.tests:TestFile.testRangedGets \
-        test.functional.tests:TestFile.testRangedGetsWithLWSinHeader \
-        test.functional.tests:TestFile.testNoContentLengthForPut \
-        test.functional.tests:TestFile.testDelete \
-        test.functional.tests:TestFile.testBadHeaders \
-        test.functional.tests:TestFile.testMetadataLengthLimits \
-        test.functional.tests:TestFile.testEtagWayoff \
-        test.functional.tests:TestFile.testFileCreate \
-        test.functional.tests:TestFile.testDeleteOfFileThatDoesNotExist \
-        test.functional.tests:TestFile.testHeadOnFileThatDoesNotExist \
-        test.functional.tests:TestFile.testMetadataOnPost \
-        test.functional.tests:TestFile.testGetOnFileThatDoesNotExist \
-        test.functional.tests:TestFile.testPostOnFileThatDoesNotExist \
-        test.functional.tests:TestFile.testMetadataOnPut \
-        test.functional.tests:TestFile.testStackedOverwrite \
-        test.functional.tests:TestFile.testTooLongName \
-        test.functional.tests:TestFile.testZeroByteFile \
-        test.functional.tests:TestFile.testEtagResponse \
-        test.functional.tests:TestDlo.test_get_manifest \
-        test.functional.tests:TestDlo.test_get_manifest_document_itself \
-        test.functional.tests:TestDlo.test_get_range \
-        test.functional.tests:TestDlo.test_get_range_out_of_range \
-        test.functional.tests:TestDlo.test_copy \
-        test.functional.tests:TestDlo.test_copy_account \
-        test.functional.tests:TestDlo.test_copy_manifest \
-        test.functional.tests:TestDlo.test_dlo_if_match_get \
-        test.functional.tests:TestDlo.test_dlo_if_none_match_get \
-        test.functional.tests:TestFileComparison \
-        test.functional.tests:TestSlo.test_slo_get_simple_manifest \
-        test.functional.tests:TestSlo.test_slo_get_nested_manifest \
-        test.functional.tests:TestSlo.test_slo_ranged_get \
-        test.functional.tests:TestSlo.test_slo_ranged_submanifest \
-        test.functional.tests:TestSlo.test_slo_etag_is_hash_of_etags \
-        test.functional.tests:TestSlo.test_slo_etag_is_hash_of_etags_submanifests \
-        test.functional.tests:TestSlo.test_slo_etag_mismatch \
-        test.functional.tests:TestSlo.test_slo_size_mismatch \
-        test.functional.tests:TestSlo.test_slo_copy \
-        test.functional.tests:TestSlo.test_slo_copy_account \
-        test.functional.tests:TestSlo.test_slo_copy_the_manifest \
-        test.functional.tests:TestSlo.test_slo_copy_the_manifest_account \
-        test.functional.tests:TestSlo.test_slo_get_the_manifest \
-        test.functional.tests:TestSlo.test_slo_head_the_manifest \
+        $SWIFT_TESTS
 
 else
     SWIFT_TEST_CONFIG_FILE=./virtualenv/etc/swift/test.conf stdbuf -oL -eL ./virtualenv/bin/nosetests -v $@
