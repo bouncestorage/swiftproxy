917f50efcc154c4276d00c400664d579bdeb13ac
Ka-Hing Cheung <khc@bouncestorage.com>
2015-06-17 12:52:39 -0700

    refactor launch swiftproxy scripts

--- src/test/resources/run-swiftclient-tests.sh
+++ src/test/resources/run-swiftclient-tests.sh
@@ -4,41 +4,9 @@ set -o xtrace
 set -o errexit
 set -o nounset
 
-PROXY_BIN="java -cp target/classes/:./target/swift-proxy-1.1.0-SNAPSHOT-jar-with-dependencies.jar com.bouncestorage.swiftproxy.Main"
-PROXY_PORT="8080"
-TEST_CONF="${PWD}/target/swiftproxy-saio.conf"
-PROXY_PID=
+source $(dirname $0)/run-swiftproxy.sh
 
-function cleanup {
-    if [ "$PROXY_PID" != "" ]; then
-        kill $PROXY_PID
-    fi
-}
-
-trap cleanup EXIT
-
-cat > target/swiftproxy-saio.conf <<EOF
-swiftproxy.endpoint=http://127.0.0.1:8080
-
-jclouds.provider=transient
-jclouds.identity=test:tester
-jclouds.credential=testing
-EOF
-
-stdbuf -oL -eL $PROXY_BIN --properties $TEST_CONF &
-PROXY_PID=$!
-
-# wait for SwiftProxy to start
-for i in $(seq 30);
-do
-    if exec 3<>"/dev/tcp/localhost/8080";
-    then
-        exec 3<&-  # Close for read
-        exec 3>&-  # Close for write
-        break
-    fi
-    sleep 1
-done
+wait_for_swiftproxy
 
 export PYTHONUNBUFFERED=1
 export NOSE_NOCAPTURE=1
