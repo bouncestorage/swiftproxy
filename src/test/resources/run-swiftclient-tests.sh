e223803a8fa19a5be4448d95d95780661bb4011f
Ka-Hing Cheung <khc@bouncestorage.com>
2015-07-22 11:10:12 -0700

    dump better error if auth fails

--- src/test/resources/run-swiftclient-tests.sh
+++ src/test/resources/run-swiftclient-tests.sh
@@ -15,6 +15,10 @@ export NOSE_NOLOGCAPTURE=1
 AUTH=$(stdbuf -oL -eL curl -i http://127.0.0.1:8080/auth/v1.0 -X GET \
     -H 'X-Auth-User: test:tester' -H 'X-Auth-Key: testing' \
     | grep -i x-storage-token | sed -e 's/.*: //')
+if [ "$AUTH" = "" ]; then
+    echo "Failed to authenticate"
+    exit 1
+fi
 
 SWIFT="swift --debug -v -v --os-auth-token $AUTH --os-storage-url http://127.0.0.1:8080/v1/AUTH_test:tester"
 
